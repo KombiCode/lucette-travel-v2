3a116baeb36361ae6f10f8b77c198d7b726961c0
Thierry Jet <phenix.tj38@gmail.com>
2021-05-12 22:35:51 +0200

    Migration to add api info (provider, poi) to Activity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_180519) do
+ActiveRecord::Schema.define(version: 2021_05_12_183953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2021_05_12_180519) do
     t.string "photo_title"
     t.float "latitude"
     t.float "longitude"
+    t.string "api_provider"
+    t.string "api_poi"
     t.index ["latitude", "longitude"], name: "index_activities_on_latitude_and_longitude"
   end
 
