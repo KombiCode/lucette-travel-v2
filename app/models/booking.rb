075b2a1783ba76d8ef63216adeb9bad3a41471cd
Thierry Jet <phenix.tj38@gmail.com>
2021-06-01 09:41:22 +0200

    Add validations for Booking. Set the right render when sve failed.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :trip
 
+  validates :name, presence: true
+  validates :address, presence: true
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
