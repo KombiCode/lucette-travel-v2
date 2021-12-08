f1e1a345fc95fd1c07487f07d5aaa9bd6ddda04e
Thierry Jet <thierry.jet@kombicode.com>
2021-12-08 23:17:19 +0100

    Improve explanation carousel management

--- app/javascript/plugins/init_carouselexplanation.js
+++ app/javascript/plugins/init_carouselexplanation.js
@@ -1,22 +1,30 @@
+const toggleDisplay = (element) => {
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
 
 const doLastClick = (e) => {
   e.preventDefault();
   const carousel = document.getElementById('carousel-explanation');
-  const formIntro = document.getElementById('trip-form-intro');
-  const form = document.getElementById('trip-form');
-  carousel.hidden = !carousel.hidden;
-  formIntro.hidden = !formIntro.hidden;
-  form.hidden = !form.hidden;
+  const form = document.getElementById('form-notrip');
+  toggleDisplay(carousel);
+  toggleDisplay(form);
 }
 
 const initCarousel = () => {
   const carousel = document.querySelector('#carousel-explanation');
+  const form = document.getElementById('form-notrip');
   const lastExplanation = document.querySelector('#last-explanation');
-
+  if (carousel && form) {
+    carousel.style.display = "block";
+    form.style.display = "none";
+  }
   if (lastExplanation) {
     lastExplanation.addEventListener('click', doLastClick);
   }
-
 };
 
 export { initCarousel };
