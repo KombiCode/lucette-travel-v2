91cb0e1012d9f66afc01ae74d5261a27303623a7
Thierry Jet <thierry.jet@kombicode.com>
2021-12-17 16:34:33 +0100

    Use an env var to instantiate either Foursquare, either Sygic as api handler

--- app/controllers/trip_activities_controller.rb
+++ app/controllers/trip_activities_controller.rb
@@ -12,13 +12,8 @@ class TripActivitiesController < ApplicationController
     @booking = @trip.bookings.find_by('begin_date <= :planned_at AND end_date >= :planned_at AND category = :category', planned_at: params[:planned_at], category: 'Hotel') || @trip.bookings.find_by(category: 'Hotel')
     @localisation = @booking&.address
 
-    # Use API to search for Points Of Interest near booking
-    # api_handler = SygicApiActivityHandler.new
-    # km_radius = 8
-    # max_pois = 64
-    # api_handler.searchActivitiesAround(@booking.latitude, @booking.longitude, km_radius, max_pois)
-
-    api_handler = FoursquareApiHandler.new
+    api_handler = (ENV['PLACES_API'] == "FOURSQUARE") ? FoursquareApiHandler.new : SygicApiActivityHandler.new
+    puts api_handler.class
     km_radius = 8
     max_pois = 25
     api_handler.searchActivitiesAround(@booking.latitude, @booking.longitude, km_radius, max_pois)
